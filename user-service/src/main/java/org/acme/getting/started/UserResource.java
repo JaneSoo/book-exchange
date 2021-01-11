8c6e7be331f6fa008081dd2a0bbc636f26eb89a3
Jan Válka <hondak57@seznam.cz>
2021-01-11 21:00:51 +0100

    managing conflits again

--- user-service/src/main/java/org/acme/getting/started/UserResource.java
+++ user-service/src/main/java/org/acme/getting/started/UserResource.java
@@ -27,26 +27,16 @@ public class UserResource {
     UserService userService;
 
     @GET
-<<<<<<< HEAD
     @Counted(name = "getAll", description = "List all the users in database")
     @Timed(name = "checksTimerGetAll", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
-=======
-    @Counted(name = "users.getAll.counter")
-    @Timed(name = "users.getAll.timer")
->>>>>>> 881800497b9cf01d3635b85a8dc239e634882162
     public List<User> getAll() {
         return User.listAll();
     }
 
     @GET
     @Path("{id}")
-<<<<<<< HEAD
     @Counted(name = "getUser", description = "Get user with given ID")
     @Timed(name = "checksTimerGet", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
-=======
-    @Counted(name = "users.get.counter")
-    @Timed(name = "users.get.timer")
->>>>>>> 881800497b9cf01d3635b85a8dc239e634882162
     public Response get(@PathParam long id) {
         User user = userService.findById(id);
 
@@ -62,39 +52,24 @@ public class UserResource {
 
     @POST
     @Path("register")
-<<<<<<< HEAD
     @Counted(name = "register", description = "Register new user to database")
     @Timed(name = "checksTimerRegister", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
-=======
-    @Counted(name = "users.create.counter")
-    @Timed(name = "users.create.timer")
->>>>>>> 881800497b9cf01d3635b85a8dc239e634882162
     public User create(User user){
         return userService.create(user);
     }
 
     @POST
     @Path("login")
-<<<<<<< HEAD
     @Counted(name = "login", description = "Verify the credential to database record whether the user is authorized")
     @Timed(name = "checksTimerLogin", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
-=======
-    @Counted(name = "users.login.counter")
-    @Timed(name = "users.login.timer")
->>>>>>> 881800497b9cf01d3635b85a8dc239e634882162
     public Long login(UserLogin login){
         return userService.login(login);
     }
 
     @PUT
     @Path("{id}")
-<<<<<<< HEAD
     @Counted(name = "update", description = "Update the record of user with given ID")
     @Timed(name = "checksTimerUpdate", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
-=======
-    @Counted(name = "users.update.counter")
-    @Timed(name = "users.update.timer")
->>>>>>> 881800497b9cf01d3635b85a8dc239e634882162
     public User update(@PathParam long id, JsonObject update){
         try{
             return userService.update(id, update);
@@ -108,13 +83,8 @@ public class UserResource {
 
     @DELETE
     @Path("{id}")
-<<<<<<< HEAD
     @Counted(name = "delete", description = "Delete user with given ID")
     @Timed(name = "checksTimerDelete", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
-=======
-    @Counted(name = "users.delete.counter")
-    @Timed(name = "users.delete.timer")
->>>>>>> 881800497b9cf01d3635b85a8dc239e634882162
     public Response delete(@org.jboss.resteasy.annotations.jaxrs.PathParam long id){
         User user;
         try{
