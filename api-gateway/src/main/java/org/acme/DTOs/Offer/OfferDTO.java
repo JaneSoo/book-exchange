b03fde8d308a70faf5179247ed009a9fff0e1973
Jan Válka <hondak57@seznam.cz>
2021-01-11 12:14:44 +0100

    gateway, fixed, added metrics and jaeger

---
+++ api-gateway/src/main/java/org/acme/DTOs/Offer/OfferDTO.java
@@ -0,0 +1,5 @@
+package org.acme.DTOs.Offer;
+
+public class Offer {
+    public Long offer_id;
+}
---
+++ borrow-service/src/main/java/org/acme/getting/started/DTOs/OfferCancelDTO.java
@@ -0,0 +1,6 @@
+package org.acme.getting.started.DTOs;
+
+public class OfferCancelDTO {
+    public long owner_id;
+    public long offer_id;
+}
